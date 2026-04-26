b02ab3dcae2e87c081f71a0bc7b553819d4ebd35
Lama <lama@gmail.com>
2026-04-26 11:15:09 +0300

    Created Navbar

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from '@mui/material/Slider';
+
+function App() {
+  return (
+    <div>
+      {}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">MyProject</a>
+        </div>
+      </nav>
+
+      {}
+      <div style={{ width: "300px", margin: "50px auto" }}>
+        <h3>Slider Example</h3>
+        <Slider defaultValue={50} valueLabelDisplay="auto" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
