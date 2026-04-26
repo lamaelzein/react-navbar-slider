bf1ef06ac580fd897fac2855fc6c43f7ca8c0258
Lama <lama@gmail.com>
2026-04-26 10:54:57 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Button from "./components/Button";
+function App() {
+  return (
+    <div >
+      <Button color="secondary" onClick={() => console.log("Clicked!")}>My Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+interface Props {
+    children: ReactNode;
+    color?: string;
+    onClick?: () => void;
+}
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+    return (
+        <button className={`btn btn-${color}`} onClick={onClick}>{children}</button>
+    );
+};
+export default Button;
